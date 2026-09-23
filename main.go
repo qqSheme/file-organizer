1893a7240f0a178ac0fb7da103d9c1615b606c1c
Vlad Avseev <vladavseev47@gmail.com>
2026-09-23 23:40:37 +0300

    1-phase

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-package fileorganizer
+package main
+
+import "fmt"
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".png":  "Images",
+	".jpeg": "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
+func main() {
+	fmt.Print(DefaultRules)
+}
